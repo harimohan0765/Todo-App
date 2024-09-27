e749e311dcfabf0fb3b8e6a5d828c8e6fa8d990e
harimohan0765 <harimohan0765@gmail.com>
2024-09-27 22:12:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let todoList = [{ item: 'Hello js', dueDate: '4/102023' },
+{ item: 'Go to collage', dueDate: '4/102023' }];
+displayItems();
+function addTodo() {
+  let inputElement = document.querySelector('#todo-input');
+  let dateElement = document.querySelector('#todo-date');
+  let todoItem = inputElement.value;
+  let todoDate = dateElement.value;
+  todoList.push({item : todoItem , dueDate:todoDate});
+
+  inputElement.value = '';
+  displayItems();
+}
+
+function displayItems() {
+  let containerElement = document.querySelector('.todo-container');
+  let newHtml = '';
+
+  for (let i = 0; i < todoList.length; i++) {
+// let item=todoList[i].item;
+// let dueDate=todoList[i].dueDate;
+let {item,dueDate}=todoList[i];
+    newHtml += `
+       
+        <span>${item}</span> 
+        <span>${dueDate}</span> 
+        <button class="button-delete " onclick="todoList.splice(${i},1);
+        displayItems();">Delete</button>
+       
+       ` ;
+
+  }
+  containerElement.innerHTML = newHtml;
+}
